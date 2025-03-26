cb106acd96b61d075714722e76b1e94fb91b4cfe
Taha Karami <tahakarami2088@gamil.com>
2025-03-26 04:39:53 -0800

    add single page theme and add style for them

--- inc/master-settings.php
+++ inc/master-settings.php
@@ -173,8 +173,8 @@ if (class_exists('CSF')) {
         'type'  => 'color',
         'title' => 'رنگ اصلی سایت',
         'output'=>array(
-          'color'=> '.phone-holder .desc',
-          'background-color'=>'.auth-btn:hover,.header-search-submit,.master-navigation>div>ul>li>a::after,.master-footer-contact .email,.master-footer-title::before',
+          'color'=> '.phone-holder .desc ,.main-page-wraper .details i',
+          'background-color'=>'.auth-btn:hover,.header-search-submit,.master-navigation>div>ul>li>a::after,.master-footer-contact .email,.master-footer-title::before,',
           'border-bottom-color'=> '.master-navigation ul.menu>li ul,ul.menu li.menu-item-has-children.mega-menu>.sub-menu'
         ),
         'output_important'=> true,
--- single.php
+++ single.php
@@ -1,13 +1,60 @@
-<?php 
-wp_head(); // برای بارگذاری هدر و اسکریپت‌ها
-get_header(); // بارگذاری هدر قالب
+<?php
+wp_head();
+get_header();
+?>
 
-if ( have_posts() ) : // بررسی اینکه آیا محتوای برگه وجود دارد
-    while ( have_posts() ) : the_post(); // شروع حلقه وردپرس
-        the_title('<h1>', '</h1>'); // نمایش عنوان برگه
-        the_content(); // نمایش محتوای برگه
-    endwhile;
-endif;
+<div class="top-section">
+    <div class="container d-flex flex-column align-items-center">
+    <div class="breadcrumbs">
+    <?php bcn_display(); ?> 
+    </div>
+ <h1 class="main-title mt-4"><?php the_title(); ?></h1>
+</div>
+</div>
 
-get_footer(); // بارگذاری فوتر قالب
+<div class="main-page-wraper single-post">
+    <div class="content-section">
+        <div class="container">
+        <article>
+            <?php while (have_posts()) : the_post(); ?>
+                <?php if (has_post_thumbnail()): ?>
+                    <div class="post-thumbnail">
+                       <?php echo the_post_thumbnail() ;?> 
+                    </div>
+                <?php endif; ?>
+                <div class="post details d-flex align-items-center justify-content-center mt-3 mb-5">
+                    <div class="d-flex align-items-center ms-3">
+                        <i class="fal fa-calendar"></i>
+                        <span>تاریخ انتشار : <?php echo get_the_date() ?></span>
+                    </div>
+                    <div class="d-flex align-items-center ms-3">
+                        <i class="fal fa-eye">
+                        </i>
+                        <span>تعداد بازدید :‌ ۵</span>
+
+                    </div>
+                    <div class="d-flex align-items-center ms-3">
+                    <i class="fal fa-user">
+                        </i>
+                        <span>نویسنده : <?php echo get_the_author()?></span>
+
+                    </div>
+                    <div class="d-flex align-items-center ms-3">
+                    <i class="fal fa-folder">
+                        </i>
+                        <span><?php echo get_the_category_list(',') ?></span>
+
+                    </div>
+                </div>
+                <div class="post-content">
+                    <?php the_content() ?> 
+                </div>
+            <?php endwhile; ?>
+        </article>
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
 ?>
